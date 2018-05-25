6e6d26d76530fd8bd9438aa75ce6ef75b563ac90
Walter Oggioni <woggioni@comelz.com>
2018-05-25 14:12:17 +0200

    added options specified from command line when using `update` of `checkout`

--- quark/checkout.py
+++ quark/checkout.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 from argparse import ArgumentParser
 from .subproject import generate_cmake_script
 from os.path import abspath, basename
+from .utils import parse_option
 from os import getcwd
 
 def run():
@@ -13,10 +14,17 @@ def run():
                         help="Specify the source directory")
     parser.add_argument("-v", "--verbose", action='store_true',
                         help="Print dependency tree in JSON format")
+    parser.add_argument("-o", "--options", action='append',
+                    help="set option value (will be taken into account when downloading optional dependencies)")
     optlist = parser.parse_args()
     url = optlist.url
+    options = {}
+    if optlist.options:
+        for option in optlist.options:
+            key, value = parse_option(option)
+            options[key] = value
     source_dir = abspath(optlist.source_directory or (optlist.url and basename(urlparse(optlist.url).path)) or getcwd())
-    generate_cmake_script(source_dir, url, print_tree=optlist.verbose)
+    generate_cmake_script(source_dir, url, print_tree=optlist.verbose, options=options)
 
 if __name__ == "__main__":
     run()
