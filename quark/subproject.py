comelz/quark

commit dff0ed393f28264aed7c5a2a6ff8c026e87ac755

Avoid rewriting CMakeLists.txt if unnecessary
A CMakeLists.txt rewrite can trigger a full rebuild; this fix tries to
avoid it unless really necessary:

- added a check to avoid rewriting a CMakeLists.txt with the exact same
  content;
- make sure to re-generate the same output for CMakeLists.txt given the
  same input; this requires some care, as Node objects and options are
  stored into set/dict, so iteration order is not deterministic (and
  indeed changes at each run). For this reason, we sort them when
  iterating to write the CMakeLists.txt file.

Incidentally, this commit slighly simplifies the generation logic (less
special cases) and gets rid of the now-unused walk_tree function.

quark/subproject.py
@@ -10,7 +10,7 @@ import shutil
 import xml.etree.ElementTree as ElementTree
 
 from quark.utils import DirectoryContext as cd, fork
-from quark.utils import freeze_file, dependency_file, mkdir, load_conf, walk_tree
+from quark.utils import freeze_file, dependency_file, mkdir, load_conf
 
 logger = logging.getLogger(__name__)
 
@@ -398,29 +398,48 @@ def generate_cmake_script(source_dir, url=None, options=None, print_tree=False,u
     conf = load_conf(source_dir)
     if update and conf is not None:
         subproject_dir = join(source_dir, conf.get("subprojects_dir", 'lib'))
-        with open(join(subproject_dir, 'CMakeLists.txt'), 'w') as cmake_lists_txt:
-            processed = set()
 
-            def dump_options(module):
-                for key, value in module.options.items():
-                    if value is None:
-                        cmake_lists_txt.write('unset(%s CACHE)\n' % (key))
-                        continue
-                    elif isinstance(value, bool):
-                        kind = "BOOL"
-                        value = 'ON' if value else 'OFF'
-                    else:
-                        kind = "STRING"
-                    cmake_lists_txt.write('set(%s %s CACHE INTERNAL "" FORCE)\n' % (key, value))
+        cmakelists_rows = []
+        processed = set()
 
-            def cb(module):
-                if (module is root or
-                    module.name in processed or 
-                    module.exclude_from_cmake or 
-                    not exists(join(module.directory, "CMakeLists.txt"))):
+        def dump_options(module):
+            for key, value in sorted(module.options.items()):
+                if value is None:
+                    cmakelists_rows.append('unset(%s CACHE)\n' % (key))
+                    continue
+                elif isinstance(value, bool):
+                    kind = "BOOL"
+                    value = 'ON' if value else 'OFF'
+                else:
+                    kind = "STRING"
+                cmakelists_rows.append('set(%s %s CACHE INTERNAL "" FORCE)\n' % (key, value))
+
+        def process_module(module):
+            if (module.name in processed or
+                module.exclude_from_cmake or
+                not exists(join(module.directory, "CMakeLists.txt"))):
+                return
+            dump_options(module)
+            if module is not root:
+                cmakelists_rows.append('add_subdirectory(%s)\n' % (module.directory))
+            processed.add(module.name)
+            # module.children is a set, whose iteration order changes from run to run
+            # make this deterministic (we want to generate always the same CMakeLists.txt)
+            for c in sorted(module.children, key = lambda x: x.name):
+                process_module(c)
+
+        process_module(root)
+
+        # write only if different
+        cmakelists_data = ''.join(cmakelists_rows)
+        try:
+            with open(join(subproject_dir, 'CMakeLists.txt'), 'r') as f:
+                if cmakelists_data == f.read():
+                    # nothing to do, avoid touching the file (which often yields a full rebuild)
                     return
-                dump_options(module)
-                cmake_lists_txt.write('add_subdirectory(%s)\n' % (module.directory))
-                processed.add(module.name)
-            dump_options(root)
-            walk_tree(root, cb)
+        except IOError:
+            pass
+        # actually write the file
+        with open(join(subproject_dir, 'CMakeLists.txt'), 'w') as f:
+            f.write(cmakelists_data)
+

quark/utils.py
@@ -25,11 +25,6 @@ def load_conf(folder):
     else:
         return None
 
-def walk_tree(root, callback):
-    for c in root.children:
-        walk_tree(c, callback)
-    callback(root)
-
 def fork(*args, **kwargs):
     sys.stdout.write(' '.join(args[0]) + '\n')
     sys.stdout.flush()

commit a4122ebe1995c14789778b8bf40f9f87929db7ad

Fixed CMakeLists.txt add_subdirectory order error introduced in dff0ed393f28264aed7c5a2a6ff8c026e87ac755
the updated process_module wrongly dumped first the given module, then
its dependencies, when the correct thing is of course the other way
around. Added some extra comments.

quark/subproject.py
@@ -411,18 +411,21 @@ def generate_cmake_script(source_dir, url=None, options=None, print_tree=False,u
                 cmakelists_rows.append('set(%s %s CACHE INTERNAL "" FORCE)\n' % (key, value))
 
         def process_module(module):
-            if (module.name in processed or
-                module.exclude_from_cmake or
-                not exists(join(module.directory, "CMakeLists.txt"))):
+            # notice: if a module is marked as excluded from cmake we also
+            # exclude its dependencies; they are nonetheless included if they
+            # are required by another module which is not excluded from cmake
+            if module.name in processed or module.exclude_from_cmake:
                 return
-            dump_options(module)
-            if module is not root:
-                cmakelists_rows.append('add_subdirectory(%s)\n' % (module.directory))
             processed.add(module.name)
+            # first add the dependent modules
             # module.children is a set, whose iteration order changes from run to run
             # make this deterministic (we want to generate always the same CMakeLists.txt)
             for c in sorted(module.children, key = lambda x: x.name):
                 process_module(c)
+            # dump options and add to the generated CMakeLists.txt
+            dump_options(module)
+            if module is not root and exists(join(module.directory, "CMakeLists.txt")):
+                cmakelists_rows.append('add_subdirectory(%s)\n' % (module.directory))
 
         process_module(root)
 