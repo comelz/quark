e109878d358540e62ce02c3f527a6025153eeedd
Matteo Italia <matteo@mitalia.net>
2018-07-31 14:25:54 +0200

    Added --deps-only switch to perform a dependency update without touching the root project
    also fix several spots to work correctly with a non-VCSed root.

--- quark/update.py
+++ quark/update.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from .subproject import generate_cmake_script, Subproject, url_from_directory
+from .subproject import generate_cmake_script, Subproject, url_from_directory, QuarkError
 from .utils import parse_option
 from os import getcwd
 
@@ -11,6 +11,8 @@ def run():
                         help="Print dependency tree in JSON format")
     parser.add_argument("-o", "--options", action='append',
                     help="set option value (will be taken into account when downloading optional dependencies)")
+    parser.add_argument("-d", "--deps-only", action='store_true',
+            help="Update only dependencies, ignore the root project")
     optlist = parser.parse_args()
     source_dir = optlist.source_directory or getcwd()
     options = {}
@@ -19,9 +21,10 @@ def run():
             key, value = parse_option(option)
             options[key] = value
 
-    root_url = url_from_directory(source_dir, include_commit = False)
-    root = Subproject.create("root", root_url, source_dir, {}, toplevel = True)
-    root.update()
+    if not optlist.deps_only:
+        root_url = url_from_directory(source_dir, include_commit = False)
+        root = Subproject.create("root", root_url, source_dir, {}, toplevel = True)
+        root.update()
     generate_cmake_script(source_dir, print_tree=optlist.verbose, options=options)
 
 if __name__ == "__main__":
