b27178625f34a2248a4dab7ea9ed56367df4e8cb
Matteo Beniamino <mbeniamino@comelz.com>
2018-08-03 12:00:36 +0200

    Fixed regression in quark status
    Now reports again the status of the root

--- quark/status.py
+++ quark/status.py
@@ -1,7 +1,7 @@
 import json
 from argparse import ArgumentParser
 from .utils import freeze_file
-from .subproject import Subproject
+from .subproject import Subproject, url_from_directory
 from os.path import join
 from os import getcwd
 
@@ -12,6 +12,7 @@ def run():
     optlist = parser.parse_args()
 
     root, modules = Subproject.create_dependency_tree(optlist.source_directory, update=False)
+    root = Subproject.create("root", url_from_directory(root.directory), root.directory, {})
     for mod in ([root] + list(modules.values())):
         print("=== Status of %s" % mod.directory)
         mod.status()
