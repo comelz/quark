ac161eb06068a77e0ed47763b93be0bf38ce804e
Francesco Cambi <fcambi@develer.com>
2019-05-03 17:32:16 +0200

    Add rev and version_control as environment variables
    SVN subprojects would not be skipped anymore

--- quark/foreach.py
+++ quark/foreach.py
@@ -15,12 +15,19 @@ def run():
         Evaluates an arbitrary shell command in each submodule, skipping all
         the svn's submodules.
         The command has access to the variables $name, $sm_path, $displaypath,
-        $sha1 and $toplevel:
+        $sha1, $toplevel, $rev, $version_control:
         $name is the name of the submodule;
         $sm_path is the path of the submodule relative to the superproject;
-        $displaypath is the path of the submodule relative to the root directory;
-        $sha1 is the commit as recorded in the immediate superproject;
-        $toplevel is the absolute path to the top-level of the immediate superproject.
+        $displaypath is the path of the submodule relative to the root
+        directory;
+        $version_control is the version control used by the subproject
+        (git/svn);
+        $sha1 is the commit of the subproject ( empty string if it is a
+        svn repository );
+        $rev is the revision of the subproject ( empty string if it is a
+        git repository );
+        $toplevel is the absolute path to the top-level of the
+        immediate superproject.
         """
     )
     parser.add_argument(
@@ -46,20 +53,7 @@ def run():
         module_displaypath = os.path.relpath(module.directory, os.getcwd())
         module_name = module.name
         toplevel = os.path.abspath(os.getcwd())
-
-        if module.get_version_control() == "git":
-            # Case for git
-            commit_sha = module.ref
-        else:
-            # Case for SVN
-            if not optlist.quiet:
-                print(
-                    "WARNING: {} at {} has been skipped because it's a "
-                    "SVN repository\n".format(
-                        module_name,
-                        module_displaypath
-                    ))
-            continue
+        version_control = module.get_version_control()
 
         cmd = " ".join([
             os.path.abspath(x) if os.path.exists(x) else x
@@ -77,12 +71,23 @@ def run():
         # the current working directory to the submodules root directory
         os.environ["displaypath"] = str(module_displaypath)
 
-        # sha1 is the commit as recorded in the immediate superproject
-        os.environ["sha1"] = str(commit_sha)
-
         # toplevel is the absolute path to the
         # top-level of the immediate superproject
         os.environ["toplevel"] = str(toplevel)
+
+        # version_control is the version control used for the subproject
+        os.environ["version_control"] = version_control
+
+        if version_control == "git":
+            # Case for git
+            # sha1 is the commit of the subproject ( empty string
+            # if it is a svn repository )
+            os.environ["sha1"] = str(module.ref)
+        else:
+            # Case for SVN
+            # rev is the revision of the subproject ( empty string if
+            # it is a git repository )
+            os.environ["rev"] = module.rev
 
         with DirectoryContext(module.directory):
             output = subprocess.check_output(cmd, shell=True)
