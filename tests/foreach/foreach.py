a288a29a9744547a8f32702297440d7a189e6474
Francesco Cambi <fcambi@develer.com>
2019-05-06 14:36:05 +0200

    tests/foreach - Print version_control and rev

--- tests/foreach/foreach.py
+++ tests/foreach/foreach.py
@@ -4,5 +4,9 @@ import os
 print("name: {}".format(os.environ["name"]))
 print("sm_path: {}".format(os.environ["sm_path"]))
 print("displaypath: {}".format(os.environ["displaypath"]))
-print("sha1: {}".format(os.environ["sha1"]))
+print("version_control: {}".format(os.environ["version_control"]))
+try:
+    print("sha1: {}".format(os.environ["sha1"]))
+except KeyError:
+    print("rev: {}".format(os.environ["rev"]))
 print("toplevel: {}".format(os.environ["toplevel"]))
--- tests/foreach/foreach.sh
+++ tests/foreach/foreach.sh
@@ -3,5 +3,7 @@
 echo "name: $name"
 echo "sm_path: $sm_path"
 echo "displaypath: $displaypath"
+echo "version_control: $version_control"
 echo "sha1: $sha1"
+echo "rev: $rev"
 echo "toplevel: $toplevel"
