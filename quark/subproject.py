4accfc058e06fcfd601602c3037eca2960f2ee95
Lorenzo Villani <lvillani@develer.com>
2024-12-30 10:27:21 +0100

    Use the maximum items-per-page limit when querying GitLab's job API
    Maximize the chance to get the job we need without having to implement
    full pagination support on the client side.
    
    Also, be explicit that we are requesting the first page.

--- quark/subproject.py
+++ quark/subproject.py
@@ -902,7 +902,7 @@ class GitlabSubproject(Subproject):
 
         # Jobs in latest pipeline
         req = urllib.request.Request(
-            url="%s/api/v4/projects/%s/pipelines/%s/jobs?scope=success&include_retried=true&per_page=50" % (
+            url="%s/api/v4/projects/%s/pipelines/%s/jobs?scope=success&include_retried=true&page=1&per_page=100" % (
                 self.gitlab_url,
                 urllib.parse.quote_plus(project),
                 pipeline["id"],
